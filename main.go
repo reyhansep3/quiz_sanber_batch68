3ddb553cf1dade2687fe0405f69c71824248317b
reyhansep3 <reyhanseptri@gmail.com>
2025-07-20 20:23:58 +0700

    fix : menyesuaikan .env menjadi .env railway

--- main.go
+++ main.go
@@ -28,11 +28,11 @@ func main() {
 
 	psqlInfo := fmt.Sprintf(
 		`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
 	)
 
 	db, err = sql.Open("postgres", psqlInfo)
